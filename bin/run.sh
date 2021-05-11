8488d60fad0660eac1d766ef6a732426d023bf65
Ronak <ronak.p@iqm.com>
2021-05-11 07:59:59 +0530

    First commit

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PATH=/home/ronak/PycharmProjects/vsn_india2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+PATH=/home/ronak/PycharmProjects/vsn_india2/vaccine_slot_notifier_india/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 now=$(date)
 echo "Timestamp: $now"
 
 . "/home/ronak/anaconda3/etc/profile.d/conda.sh"
 conda activate vsn
 
-cd /home/ronak/PycharmProjects/vsn_india2
+cd /home/ronak/PycharmProjects/vsn_india2/vaccine_slot_notifier_india
 source env.sh
 python -m main
