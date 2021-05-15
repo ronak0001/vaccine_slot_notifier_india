66c90181cc192feba65d36735fb347a124844306
Ronak <ronak.p@iqm.com>
2021-05-15 13:02:32 +0530

    Bump version: 1.0.3 → 1.0.4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 __author__ = "Ronak Prajapati"
 __copyright__ = "Copyright 2021, RKP, Vaccine Slot Notifier India"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __maintainer__ = "Ronak Prajapati"
 __email__ = "prajapatironak12@gmail.com"
 __status__ = "Development"
